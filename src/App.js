eed82cc8e7d0aaebd64eb47d08b1be098b6f12d3
Abdullah Al Sayad <108291857+Sayadkhan@users.noreply.github.com>
2023-05-05 23:56:27 +0600

    router setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Favourites from "./components/Favourites";
+import NotFoundPage from "./pages/NotFoundPage";
 
 const App = () => {
-  return <div className="text-9xl">App</div>;
+  return (
+    <>
+      <div className="app">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favourites" element={<Favourites />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </div>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
